d8a1b082197d6341feb529275cd1817b13318aea
mesoibum joon kim <omjoonkim@gmail.com>
2016-11-19 16:13:25 +0900

    firstCommit

---
+++ DoYouRemember/app/src/main/java/com/omjoonkim/doyouremember/realm/Person.java
@@ -0,0 +1,19 @@
+package com.omjoonkim.doyouremember.realm;
+
+
+import java.util.List;
+
+
+public class Person {
+
+	long id;
+
+	String name;
+
+	List<Account> accountList;
+
+	boolean isMine;
+
+
+
+}
