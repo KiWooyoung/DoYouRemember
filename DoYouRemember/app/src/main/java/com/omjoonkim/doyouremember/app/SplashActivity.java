432e26b104691e8d76c551208d57863b870c06d5
KIM-JUNG-MIN <ctl_anna17@naver.com>
2017-03-26 07:54:48 +0900

    feature : splash화면 및 앱 로고 적용

--- DoYouRemember/app/src/main/java/com/omjoonkim/doyouremember/app/SplashActivity.java
+++ DoYouRemember/app/src/main/java/com/omjoonkim/doyouremember/app/SplashActivity.java
@@ -1,18 +1,31 @@
 package com.omjoonkim.doyouremember.app;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.omjoonkim.doyouremember.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.splash_title)
+    TextView splash_title;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ButterKnife.bind(this);
+
+        Typeface typeFace1 = Typeface.createFromAsset(getAssets(), "야놀자 야체 OTF Regular.otf");
+        splash_title.setTypeface(typeFace1);
 
         findViewById(R.id.splash).postDelayed(new Runnable() {
             @Override
